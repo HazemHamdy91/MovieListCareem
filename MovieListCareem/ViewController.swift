ef392f95f06e522078048e23593798a583d75a0e
Hazem Hamdy <hazemhamdy@Hazems-MacBook.local>
2018-08-31 17:24:46 +0200

    Initial Commit

---
+++ MovieListCareem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieListCareem
+//
+//  Created by Hazem Hamdy on 8/31/18.
+//  Copyright © 2018 Hazem Hamdy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
