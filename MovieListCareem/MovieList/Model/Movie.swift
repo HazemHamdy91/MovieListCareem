c70155247da61406bb9f41ecaab7dbfd0f8608fc
Hazem Hamdy <hazemhamdy@Hazems-MacBook.local>
2018-08-31 17:43:56 +0200

    Adding Movie List Layers

---
+++ MovieListCareem/MovieList/Model/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  MovieListCareem
+//
+//  Created by Hazem Hamdy on 8/31/18.
+//  Copyright © 2018 Hazem Hamdy. All rights reserved.
+//
+
+import UIKit
+
+class Movie: NSObject {
+
+}
